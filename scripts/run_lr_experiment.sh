ccdea1182fa613ed1bb75de345adf1feb6eb3f5e
DanTimson <dantimsonwork@gmail.com>
2026-03-24 20:43:17 +0300

    new config

--- scripts/run_lr_experiment.sh
+++ scripts/run_lr_experiment.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./scripts/run_config.sh configs/muon_lora_lr3e3.yaml
-./scripts/run_config.sh configs/muon_lora_lr1e2.yaml
+./scripts/run_config.sh configs/adamw_lora_lr3e3.yaml
+
+./scripts/run_config.sh configs/muon_lora_lr3e4.yaml
+./scripts/run_config.sh configs/muon_lora_lr1e3.yaml
